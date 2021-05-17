372dd889ae82e2a95815916b21474a7fd5f652df
Berthold Stoeger <bstoeger@mail.tuwien.ac.at>
2021-05-17 19:28:53 +0200

    cleanup: remove unused function setToolTipVisibile()
    The alternative spelling "visibile" made searching for this
    function very annoying. That makes removing it even more
    satisfying.
    
    Signed-off-by: Berthold Stoeger <bstoeger@mail.tuwien.ac.at>

--- profile-widget/profilewidget2.cpp
+++ profile-widget/profilewidget2.cpp
@@ -1230,11 +1230,6 @@ void ProfileWidget2::setProfileState()
 }
 
 #ifndef SUBSURFACE_MOBILE
-void ProfileWidget2::setToolTipVisibile(bool visible)
-{
-	toolTipItem->setVisible(visible);
-}
-
 void ProfileWidget2::connectPlannerModel()
 {
 	connect(plannerModel, &DivePlannerPointsModel::dataChanged, this, &ProfileWidget2::replot);
